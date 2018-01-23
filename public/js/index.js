6a3c708491d4ed8721b55d8107c9d0b0911e73a7
HanVincent <vincent.han.tw@gmail.com>
2018-01-23 20:56:07 +0800

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,22 @@
 
 const socket = io();
 
+function scrollToBottom() {
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+        console.log("D");
+    }
+}
+
 socket.on('connect', function () {
     console.log('connected to server');
 });
@@ -19,6 +35,7 @@ socket.on('newMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -31,6 +48,7 @@ socket.on('newLocationMessage', function (message) {
     });
 
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 const textbox = $('[name=message]');
