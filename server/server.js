6b743f497b02ec7db146401fb42479d417f0b8ce
HanVincent <vincent.han.tw@gmail.com>
2018-11-29 22:34:54 +0900

    Update package.json and comment some codes in server

--- server/server.js
+++ server/server.js
@@ -19,6 +19,7 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
     console.log('New user connected');
+
     socket.on('join', (params, callback) => {
         if (!params.name || !params.room) {
             return callback('Name and room are required!');
@@ -34,29 +35,30 @@ io.on('connection', (socket) => {
         // socket.broadcast.emit -> socket.broadcast.to('The office').emit
         // socket.emit
 
-        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.emit('newMessage', generateMessage('Admin', 'Considering security problem, only show the chatroom prototype.'));
         socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} join`));
         callback();
     });
 
-    socket.on('createMessage', (message, callback) => {
-        const user = users.getUser(socket.id);
+    // Comment the codes in case of security.
+    // socket.on('createMessage', (message, callback) => {
+    //     const user = users.getUser(socket.id);
 
-        if (user && isRealString(message.text)) {
-            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
-        }
+    //     if (user && isRealString(message.text)) {
+    //         io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+    //     }
 
-        callback('This text is from server');
-    });
+    //     callback('This text is from server');
+    // });
 
-    socket.on('createLocationMessage', (coords, callback) => {
-        const user = users.getUser(socket.id);
+    // socket.on('createLocationMessage', (coords, callback) => {
+    //     const user = users.getUser(socket.id);
 
-        if (user) {
-            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
-        }
+    //     if (user) {
+    //         io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+    //     }
 
-    });
+    // });
 
     socket.on('disconnect', () => {
         const user = users.removeUser(socket.id);
